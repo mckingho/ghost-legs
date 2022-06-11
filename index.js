3d71a88ada0cebd70c5fa50d4e9a64f79415cba7
Michael Cheung <mc.kingho@gmail.com>
2022-06-11 17:08:58 +0800

    Add column feature

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import {
     heads,
     getColumns,
     getRows,
-    reset,
     addSwap,
     removeSwap,
     addColumn,
@@ -28,55 +27,75 @@ function updateBoard() {
     let rows = getRows();
     for (let lv = 0; lv < rows; lv += 1) {
         let row = table.insertRow(lv);
+        row.setAttribute("id", "row-" + lv);
         for (let c = 0; c < columns; c += 1) {
-            let idx = c * rows + lv;
-            let cell = row.insertCell(c);
-            if (c < columns - 1) {
-                // no swap display at last column
-                cell.classList.add("no-swap");
-            }
-            cell.setAttribute("id", "cell-" + idx);
-            cell.addEventListener("click", function (event) {
-                handleClickSwap(event, c, lv);
-            });
+            addCell(row, c, lv);
         }
     }
 
     let foot = table.createTFoot();
     let footRow = foot.insertRow(0);
+    footRow.setAttribute("id", "row-foot");
     for (let c = 0; c < feet.length; c += 1) {
-        let cell = footRow.insertCell(c);
-        cell.classList.add("foot-cell");
-        cell.setAttribute("id", "foot-cell-" + c);
-        let input = document.createElement("input");
-        input.setAttribute("type", "text");
-        input.setAttribute("id", "input-foot-" + c);
-        input.value = feet[c];
-        cell.appendChild(input);
+        addFootCell(footRow, c);
     }
 
     let head = table.createTHead();
     let headRow = head.insertRow(0);
+    headRow.setAttribute("id", "row-head");
     for (let c = 0; c < heads.length; c += 1) {
-        let cell = headRow.insertCell(c);
-        cell.classList.add("head-cell");
-        cell.setAttribute("id", "head-cell-" + c);
-        let input = document.createElement("input");
-        input.setAttribute("type", "text");
-        input.setAttribute("id", "input-head-" + c);
-        input.value = heads[c];
-        cell.appendChild(input);
-        let runBtn = document.createElement("button");
-        runBtn.classList.add("run-btn");
-        runBtn.innerHTML = "RUN";
-        cell.appendChild(runBtn);
-        runBtn.addEventListener("click", function (event) {
-            handleRun(event, c);
-        });
+        addHeadCell(headRow, c);
     }
     let cell = headRow.insertCell(heads.length);
     cell.innerHTML = "+";
     cell.classList.add("head-plus-cell");
+    cell.addEventListener("click", function (event) {
+        handleAddColumn(event);
+    })
+}
+
+function addCell(row, c, lv) {
+    let columns = getColumns();
+    let rows = getRows();
+    let idx = c * rows + lv;
+    let cell = row.insertCell(c);
+    if (c < columns - 1) {
+        // no swap display at last column
+        cell.classList.add("no-swap");
+    }
+    cell.setAttribute("id", "cell-" + idx);
+    cell.addEventListener("click", function (event) {
+        handleClickSwap(event, c, lv);
+    });
+}
+
+function addFootCell(footRow, c) {
+    let cell = footRow.insertCell(c);
+    cell.classList.add("foot-cell");
+    cell.setAttribute("id", "foot-cell-" + c);
+    let input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "input-foot-" + c);
+    input.value = feet[c];
+    cell.appendChild(input);
+}
+
+function addHeadCell(headRow, c) {
+    let cell = headRow.insertCell(c);
+    cell.classList.add("head-cell");
+    cell.setAttribute("id", "head-cell-" + c);
+    let input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "input-head-" + c);
+    input.value = heads[c];
+    cell.appendChild(input);
+    let runBtn = document.createElement("button");
+    runBtn.classList.add("run-btn");
+    runBtn.innerHTML = "RUN";
+    cell.appendChild(runBtn);
+    runBtn.addEventListener("click", function (event) {
+        handleRun(event, c);
+    });
 }
 
 function updateBoardSize() {
@@ -117,6 +136,8 @@ function handleClickSwap(event, col, row) {
 
 function handleRun(event, col) {
     try {
+        resetRun();
+
         let { cellsRoute: route, colEnd } = runRoute(col);
         for (let idx = 0; idx < route.length; idx += 1) {
             let cell = document.getElementById("cell-" + idx);
@@ -152,6 +173,30 @@ function resetRun() {
     });
 }
 
+function handleAddColumn() {
+    resetRun();
+
+    addColumn();
+
+    let columns = getColumns();
+    let newCol = columns - 1;
+
+    let rows = getRows();
+    for (let lv = 0; lv < rows; lv += 1) {
+        let row = document.getElementById("row-" + lv);
+        addCell(row, newCol, lv);
+        // set swap display to previouly last column
+        let idx = (newCol - 1) * rows + lv;
+        let prevCell = document.getElementById("cell-" + idx);
+        prevCell.classList.add("no-swap");
+    }
+    let footRow = document.getElementById("row-foot");
+    addFootCell(footRow, newCol);
+    let headRow = document.getElementById("row-head");
+    addHeadCell(headRow, newCol);
+
+    updateBoardSize();
+}
 
 window.onresize = updateBoardSize;
 window.update = update;
